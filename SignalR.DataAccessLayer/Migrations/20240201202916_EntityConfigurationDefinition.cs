2c8361a6c4a9a19c12a58c94b121c3df02edf501
umutatras <umutatras@gmail.com>
2024-02-01 23:29:49 +0300

    EntityConfiguration Database migration

---
+++ SignalR.DataAccessLayer/Migrations/20240201202916_EntityConfigurationDefinition.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalR.DataAccessLayer.Migrations
+{
+    public partial class EntityConfigurationDefinition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Testimonials_Id",
+                table: "Testimonials",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialMedias_Id",
+                table: "SocialMedias",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Id",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Features_Id",
+                table: "Features",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Discounts_Id",
+                table: "Discounts",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_Id",
+                table: "Contacts",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Abouts_Id",
+                table: "Abouts",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Testimonials_Id",
+                table: "Testimonials");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SocialMedias_Id",
+                table: "SocialMedias");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Id",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Features_Id",
+                table: "Features");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Discounts_Id",
+                table: "Discounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_Id",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Abouts_Id",
+                table: "Abouts");
+        }
+    }
+}
