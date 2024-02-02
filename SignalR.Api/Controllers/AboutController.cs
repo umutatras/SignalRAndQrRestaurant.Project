be39c74417d2edd1effbbe13c0545fb00b847beb
umutatras <umutatras@gmail.com>
2024-02-02 23:09:03 +0300

    aboutcontroller crud endpointler yazıldı

--- SignalR.Api/Controllers/AboutController.cs
+++ SignalR.Api/Controllers/AboutController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Interfaces;
 using SignalR.CommonLayer;
 using SignalR.DtoLayer.AbotDtos;
+using SignalR.DtoLayer.Interfaces;
 
 namespace SignalR.Api.Controllers
 {
@@ -20,9 +21,32 @@ namespace SignalR.Api.Controllers
         [HttpGet]
         public async Task<IResponse<List<AboutListDto>>> GetAboutList()
         {
-            var result=await _aboutService.GetAllAsync();
+            var result = await _aboutService.GetAllAsync();
             return result;
         }
-
+        [HttpGet]
+        public async Task<IResponse<AboutListDto>> GetById(int id)
+        {
+            var result = await _aboutService.GetByIdAsync<AboutListDto>(id);
+            return result;
+        }
+        [HttpPost]
+        public async Task<IResponse<AboutCreateDto>> Add(AboutCreateDto dto)
+        {
+            var result = await _aboutService.CreateAsync(dto);
+            return result;
+        }
+        [HttpPut]
+        public async Task<IResponse<AboutUpdateDto>> Update(AboutUpdateDto dto)
+        {
+            var result = await _aboutService.UpdateAsync(dto);
+            return result;
+        }
+        [HttpDelete]
+        public async Task<IResponse> Remove(int id)
+        {
+            var result = await _aboutService.RemoveAsync(id);
+            return result;
+        }
     }
 }
