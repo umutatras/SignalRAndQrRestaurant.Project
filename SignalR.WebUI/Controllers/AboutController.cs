5cc02440a4766d8b13141e5e91af20bab33fb693
umutatras <umutatras@gmail.com>
2024-02-07 13:16:53 +0300

    Yeni tema import edildi.AboutController yazıldı

---
+++ SignalR.WebUI/Controllers/AboutController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.WebUI.Dtos;
+using System.Text;
+
+namespace SignalR.WebUI.Controllers;
+
+public class AboutController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AboutController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7250/api/about/GetAboutList");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResponseDto<List<AboutListDto>>>(jsonData);
+            return View(values.Data);
+        }
+        return View();
+    }
+
+    public IActionResult CreateAbout()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateAbout(AboutCreateDto dto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(dto);
+        StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PostAsync("https://localhost:7250/api/about/add", content);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View(dto);
+    }
+    public async Task<IActionResult> DeleteAbout(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.DeleteAsync($"https://localhost:7250/api/about/remove?id={id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    public async Task<IActionResult> UpdateAbout(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync($"https://localhost:7250/api/about/getbyid?id={id}");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<ResponseDto<AboutUpdateDto>>(jsonData);
+            return View(values.Data);
+        }
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> UpdateAbout(AboutUpdateDto dto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(dto);
+        StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responseMessage = await client.PutAsync("https://localhost:7250/api/about/update", content);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+        return View(dto);
+    }
+}
---
+++ SignalR.WebUI/Dtos/About/AboutListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.WebUI.Dtos;
+
+public class AboutListDto
+{
+    public int Id { get; set; }
+    public string ImageUrl { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
